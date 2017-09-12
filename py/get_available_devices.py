75663d473ff3c584ccd2a34f4e6a4a9ca5889af0
Stuart Hagler <s.hagler.2015@ieee.org>
2017-09-12 11:00:40 -0700

    Refactoring

---
+++ py/get_available_devices.py
@@ -0,0 +1,9 @@
+from tensorflow.python.client import device_lib
+
+def get_available_cpus():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos if x.device_type == 'CPU']
+
+def get_available_gpus():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos if x.device_type == 'GPU']
